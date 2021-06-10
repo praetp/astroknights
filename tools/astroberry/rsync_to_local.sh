32a62b9ea0f797d0b528a2e8479367666ff812bf
Paul Praet <3198728+praetp@users.noreply.github.com>
2021-06-10 23:39:35 +0200

    tools update

---
+++ tools/astroberry/rsync_to_local.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Copies files from astroberry to your home directory
+#run as DEST=/astrophotography/projects/M39/2021-06-09 ./rsync_to_local.sh 
+set -e
+while true
+do
+	rsync -av --remove-source-files astroberry:from_camera $DEST
+	sleep 1
+done
+
