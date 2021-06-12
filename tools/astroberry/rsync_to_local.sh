1da6dab1dd2f93eefe6f7eb199df28df4fb614b5
Paul Praet <3198728+praetp@users.noreply.github.com>
2021-06-12 10:01:13 +0200

    Failure in sync script should not be fatal

--- tools/astroberry/rsync_to_local.sh
+++ tools/astroberry/rsync_to_local.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #Copies files from astroberry to your home directory
 #run as DEST=/astrophotography/projects/M39/2021-06-09 ./rsync_to_local.sh 
-set -e
 while true
 do
 	rsync -av --remove-source-files astroberry:from_camera $DEST
