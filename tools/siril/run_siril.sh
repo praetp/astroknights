42cd5414601ce70e090a943e05f69e9ae11da3ce
Paul Praet <3198728+praetp@users.noreply.github.com>
2021-06-25 13:32:55 +0200

    Wrapper script around Siril

---
+++ tools/siril/run_siril.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#Partially inspired by https://davidmoulton.me/astronomy/astrophotography/2020/08/02/astrophotography-with-free-software.html
+set -xEeuo pipefail
+
+trap "rm -rf process" ERR
+
+#This script will process flats, darks and lights.
+#It's not a problem if flats and/or darks are missing
+#There is no support for biases currently, we use a hardcoded synthetic bias instead.
+#If the input directories are named incorrectly (e.g. after copying from EKOS), this is not a problem.
+
+#For now this script must be run from the directory where the Lights/ Flats/ .. directories are
+SCRIPT_DIR=$(realpath $BASH_SOURCE)/../scripts
+
+
+MASTER_FLAT_ARG=""
+MASTER_DARK_ARG=""
+
+#make a separate directory for master calibration frames.
+if [ ! -d "masters" ]; then
+	mkdir masters
+fi
+
+if [ -d "Flats" ]; then
+	ln -sf Flats flats
+fi
+
+if [ -f "masters/masterFlat.fit" ]; then
+	echo "Reusing masterFlat"
+	MASTER_FLAT_ARG="-flat=../masters/masterFlat"
+elif [ -e "flats" ]; then
+	siril -s ${SCRIPT_DIR}/makeMasterFlat.ssf
+	rm -rf process
+	MASTER_FLAT_ARG="-flat=../masters/masterFlat"
+else
+	echo "No flats to process. You may have vignetting and unwanted artifacts in the final result."
+fi
+
+if [ -d "Darks" ]; then
+	ln -sf Darks darks
+fi
+
+if [ -f "masters/masterDark.fit" ]; then
+	echo "Reusing masterDark"
+	MASTER_DARK_ARG="-dark=../masters/masterDark"
+elif [ -e "darks" ]; then
+	siril -s ${SCRIPT_DIR}/makeMasterDark.ssf
+	rm -rf process
+	MASTER_DARK_ARG="-dark=../masters/masterDark"
+else
+	echo "No darks to process. You may have elavated noise levels and/or hot pixes in the final result."
+fi
+
+if [ -d "Lights" ]; then
+	ln -sf Lights lights
+fi
+
+
+siril -s - <<END_OF_SCRIPT
+requires 0.99.10
+
+# Convert Light Frames to .fit files
+cd lights
+convert light -out=../process
+cd ../process
+
+# Pre-process Light Frames
+#PAUL: NOT SURE IF WE CAN ALWAYS ADD BIAS HERE... OR ONLY if you don't use flats ?
+preprocess light -bias="=2048" ${MASTER_DARK_ARG} ${MASTER_FLAT_ARG} -cfa -equalize_cfa -debayer
+
+# Align lights
+register pp_light
+
+# Stack calibrated lights to result.fit
+stack r_pp_light rej 3 3 -norm=addscale -output_norm -out=../result
+
+cd ..
+close
+
+END_OF_SCRIPT
+rm -rf process
+echo "Processing completed successfully"
