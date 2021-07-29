349da21f4eb3f7285162622a3cf1ef0ac67d25c4
Paul Praet <3198728+praetp@users.noreply.github.com>
2021-07-29 11:21:02 +0200

    Small fix for sync script

--- tools/astroberry/rsync_to_local.sh
+++ tools/astroberry/rsync_to_local.sh
@@ -2,10 +2,12 @@
 #Copies files from astroberry to your home directory
 #run as DEST=/astrophotography/projects/M39/2021-06-09 ./rsync_to_local.sh 
 DEST=${DEST:-/tmp}
+rsync -av --remove-source-files astroberry:/camera $DEST
 while true
 do
 	#This has spurious failures so better not fail the script when it fails
-	rsync -av --remove-source-files astroberry.local:/camera $DEST
+	#do not remove source files as it may remove files that are not complete yet
+	rsync -av astroberry:/camera $DEST
 	sleep 1
 done
 
