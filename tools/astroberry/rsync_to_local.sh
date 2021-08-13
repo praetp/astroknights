735bd88772ede796612684cc9da8f48b2d62c85b
Paul Praet <3198728+praetp@users.noreply.github.com>
2021-08-13 09:31:21 +0200

    fix sync script

--- tools/astroberry/rsync_to_local.sh
+++ tools/astroberry/rsync_to_local.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+set -e
 #Copies files from astroberry to your home directory
 #run as DEST=/astrophotography/projects/M39/2021-06-09 ./rsync_to_local.sh 
 DEST=${DEST:-/tmp}
 while true
 do
-	#This has spurious failures so better not fail the script when it fails
 	rsync -av astroberry:/camera $DEST
-	#remove files older than a minute
-	ssh astroberry "find /path -mmin +59 -type f -exec rm -fv {} \;"
+	#remove files older than 5 minutes
+	ssh astroberry "find /camera -mmin +5 -type f -exec rm -fv {} \;"
 	sleep 1
 done
 
