c06c6b52909f102c540205bb90bfecb83011572c
Paul Praet <3198728+praetp@users.noreply.github.com>
2023-01-28 22:08:16 +0100

    updated cleanup script

--- tools/astroberry/cleanup.sh
+++ tools/astroberry/cleanup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+set -ex
 if ping -q -c 1 google.be; then
-	find -L ~/logs -mindepth 1 -mtime +2 -delete
+	#cleanup all logs
+	find -L ~/logs/* -mindepth 1 -mtime +2 -delete
+	#cleanup old empty dirs and files
+	find /camera/ -type d -empty -print -delete -o -type f -empty -print -delete
+
 	logger "Cleanup done" 
 else
 	logger "Cannot reach google - probably datetime not correct then. Not cleaning..." 
