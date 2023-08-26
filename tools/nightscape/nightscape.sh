6d5a4ab90cd38b0870cb234581434356d250fcc4
Paul Praet <3198728+praetp@users.noreply.github.com>
2023-08-26 10:00:29 +0200

    update nightscape script

--- tools/nightscape/nightscape.sh
+++ tools/nightscape/nightscape.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -xe
 VCODEC=libx265 #software
-VCODEC=hevc_nvenc #hardware
+#VCODEC=hevc_nvenc #hardware
 #ffmpeg -y -framerate 15 -pattern_type glob -i '*.jpg' -pix_fmt yuv420p -vcodec libx265 -crf 28 out.mp4
 #ffmpeg -y -framerate 5 -pattern_type glob -i '*.jpg' -pix_fmt yuv420p out.mp4
-ffmpeg -y -framerate 15 -pattern_type glob -i '*.jpg' -pix_fmt yuv420p -vcodec ${VCODEC} out.mp4
+ffmpeg -y -framerate 60 -pattern_type glob -i '*.JPG' -pix_fmt yuv420p -vcodec ${VCODEC} -crf 20 out.mp4
 echo "Conversion done" 
