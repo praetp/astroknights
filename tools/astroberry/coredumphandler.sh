46bf66ee9b85b8a08752de24fc39bf2995bf1d4a
Paul Praet <3198728+praetp@users.noreply.github.com>
2025-06-26 18:30:58 +0200

    add small remark

--- tools/astroberry/coredumphandler.sh
+++ tools/astroberry/coredumphandler.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -x
 #this coredump handler will first park the mount and then call systemd-coredump
 
-cd ${BASH_SOURCE[0]:-$0}
-
 #should we check for kstars coredump or we just execute this for any crash ? Probaly good enough for now
 
 logger "Crash detected! $(pwd)"
 logger "Mount parking" 
+#in fact not really needed if you use the indi watchdog
 indi_setprop -h localhost EQMod\ Mount.TELESCOPE_PARK.PARK=On
 logger "Calling systemd-coredump"
 exec /usr/lib/systemd/systemd-coredump "$@" 
